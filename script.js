3ae89135f9f6de2398e00353a5a106fededb4d4e
Helder-Lucau <helderlucau@gmail.com>
2025-02-09 19:38:12 +0100

    fix carousel not displaying due to jquery not loading correctly

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-const menuBtn = document.getElementById('nav-menu')
-const navLinks = document.getElementById('navbar-links')
-const menuBtnIcon = menuBtn.querySelector('i')
-
-menuBtn.addEventListener('click', (e) => {
-    navLinks.classList.toggle('open')
-
-    const isOpen = navLinks.classList.contains('open')
-    menuBtnIcon.setAttribute('class', isOpen ? "ri-close-line" : 'ri-menu-line')
-})
-
-navLinks.addEventListener('click', (e) => {
-    navLinks.classList.remove('open')
-    menuBtnIcon.setAttribute('class', 'ri-menu-line')
-})
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        responsive: {
+            0: {
+                items: 1
+            },
+            900: {
+                items: 2
+            },
+        }
+    });
+});
