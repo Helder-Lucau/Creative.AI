e3ccb979d64def3b81f5f600fe1eac722b2ebd0b
Helder-Lucau <helderlucau@gmail.com>
2025-02-15 20:23:16 +0100

    countdown animation with plus sign for project count

--- script.js
+++ script.js
@@ -15,3 +15,22 @@ $(document).ready(function () {
         }
     });
 });
+
+// Number counting animation
+let valueDisplays = document.querySelectorAll(".countdown")
+let interval = 2000
+valueDisplays.forEach((display) => {
+    let startValue = 0;
+    let endValue = parseFloat(display.getAttribute("data-val"));
+    endValue = Math.round(endValue)
+    let duration = Math.floor(interval / endValue);
+
+    // Start the countdown animation from 0 to the given value
+    let counter = setInterval(function () {
+        startValue += 1;
+        display.textContent = `${startValue}+`;
+        if (startValue == endValue) {
+            clearInterval(counter);
+        }
+    }, duration)
+})
